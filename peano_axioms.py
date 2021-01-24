1d4baffc253747442eff2e50a6684cd5e684fb75
Abhinand Shibu <abhinandshibu@outlook.com>
2021-01-24 21:46:35 +0000

    Peano Axiom 2 Half-Done, added n, lacks n++

--- peano_axioms.py
+++ peano_axioms.py
@@ -8,12 +8,12 @@ class Peano(Scene):
         }
     
     def construct(self):
-        self.intro()
+        # self.intro()
         self.axiom1()
         self.axiom2()
-        self.axiom3()
-        self.axiom4()
-        self.axiom5()
+        # self.axiom3()
+        # self.axiom4()
+        # self.axiom5()
 
     def intro(self):
         # Peano Axioms Title
@@ -56,9 +56,45 @@ class Peano(Scene):
             MoveToTarget(zeroDot),
             run_time = 0.5
         )
+        self.wait(0.5)
 
     def axiom2(self):
-        pass
+        # Add axiom text
+        axiom2 = VGroup(
+            TextMobject("2. If n is a natural number, then n++ "),
+            TextMobject("is also a natural number")
+        ).arrange(DOWN)
+        self.play(Write(axiom2))
+        
+        # Move axiom text to top left
+        axiom2.generate_target()
+        axiom2.target.arrange(DOWN, aligned_edge=LEFT).scale(0.75).shift(3.7*LEFT + 2.2*UP).set_color(self.b)
+        self.play(
+            MoveToTarget(axiom2, run_time=1)
+        )
+        self.wait(0.5)
+
+        # Add n dot and corresponding text
+        nDot = Dot().move_to(2*RIGHT).set_color(self.g)
+        nText = TextMobject("n").move_to(3*DOWN + 2*RIGHT).set_color(self.g)
+        self.play(
+            Write(nDot), 
+            run_time = 0.5
+            )
+        self.wait(0.2)
+        self.play(
+            Write(nText), 
+            run_time = 0.3
+            )
+
+        # Move n dot down to text
+        nDot.generate_target()
+        nDot.target.move_to(2.5*DOWN + 2*RIGHT)
+        self.play(
+            MoveToTarget(nDot),
+            run_time = 0.5
+        )
+        self.wait(0.5)
 
     def axiom3(self):
         pass

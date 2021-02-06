eeaa5758beb3cef60d444d8a05d40ea2d7fd0eed
Abhinand Shibu <abhinandshibu@outlook.com>
2021-02-06 23:44:28 +0000

    Added grid (from elteoremadebeethoven), png of grid, deleted pythagorus_theorum.py, refactored peano axiom 1

--- peano_axioms.py
+++ peano_axioms.py
@@ -1,16 +1,23 @@
 from manimlib.imports import *
+from tools.grid import ScreenGrid
+
 
 class Peano(Scene):
     CONFIG = {
-            "r":COLOR_MAP["RED_D"],
-            "g":COLOR_MAP["GREEN_D"],
-            "b":COLOR_MAP["BLUE_D"]
-        }
-    
+        "r": COLOR_MAP["RED_D"],
+        "g": COLOR_MAP["GREEN_D"],
+        "b": COLOR_MAP["BLUE_D"],
+        "y_of_dots": 2.5 * DOWN,
+        "y_of_nums": 3 * DOWN
+    }
+
     def construct(self):
+        grid = ImageMobject('tools/grid.png').scale(4)
+        self.add(grid)
+
         # self.intro()
         self.axiom1()
-        self.axiom2()
+        # self.axiom2()
         # self.axiom3()
         # self.axiom4()
         # self.axiom5()
@@ -24,24 +31,26 @@ class Peano(Scene):
         self.wait(0.5)
 
     def axiom1(self):
-        # Add dot and text representing 0
-        zeroDot = Dot()
-        zeroNum = TextMobject("0").move_to(3 * DOWN)
+        # Add dot representing 0
+        zero_dot = Dot()
         self.play(
-            Write(zeroDot),
+            Write(zero_dot),
             run_time=0.5
         )
         self.wait(0.2)
+
+        # Add text representing 0
+        zero_text = TextMobject("0").move_to(self.y_of_nums)
         self.play(
-            Write(zeroNum),
+            Write(zero_text),
             run_time=0.3
         )
 
-        # Move zero dot down to text
-        zeroDot.generate_target()
-        zeroDot.target.move_to(2.5 * DOWN)
+        # Move zero dot down to zero text
+        zero_dot.generate_target()
+        zero_dot.target.move_to(self.y_of_dots)
         self.play(
-            MoveToTarget(zeroDot),
+            MoveToTarget(zero_dot),
             run_time=1
         )
         self.wait(0.7)
@@ -50,10 +59,11 @@ class Peano(Scene):
         axiom1 = TextMobject("1. 0 is a natural number")
         self.play(Write(axiom1))
         self.wait(1)
-        
+
         # Move axiom text to top left
         axiom1.generate_target()
-        axiom1.target.scale(0.75).shift(4.8*LEFT + 3*UP).set_color(self.b)
+        axiom1.target.scale(0.75).move_to(6.5*LEFT + 3.5*UP,
+                                          aligned_edge=UP+LEFT).set_color(self.b)
         self.play(
             MoveToTarget(axiom1),
             run_time=1.65
@@ -87,20 +97,16 @@ class Peano(Scene):
         axiom2 = TextMobject("2. If n is a natural number, then n++ "),
 
         self.play(Write(axiom2))
-        
+
         # Move axiom text to top left
         axiom2.generate_target()
-        axiom2.target.arrange(DOWN, aligned_edge=LEFT).scale(0.75).shift(3.7*LEFT + 2.2*UP).set_color(self.b)
+        axiom2.target.arrange(DOWN, aligned_edge=LEFT).scale(0.75).shift(3.7 * LEFT + 2.2 * UP).set_color(self.b)
         self.play(
             MoveToTarget(axiom2, run_time=1)
         )
         self.wait(0.5)
 
-
-
-        # 
-
-        
+        #
 
     def axiom3(self):
         pass

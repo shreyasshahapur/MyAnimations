5495bed1d5964b11824d0b9b21273c95a19b6c4b
Abhinand Shibu <abhinandshibu@outlook.com>
2021-02-12 20:59:37 +0000

    added functionality for adding labels, in need of refactoring and documentation.

--- tools/basic_graph.py
+++ tools/basic_graph.py
@@ -4,12 +4,14 @@ from manim import *
 class BasicGraph(VMobject):
 
     def __init__(self,
-                 vertices={},  # includes position
+                 vertices={},  # includes position {"A": [1, 0], "B": [-1, -1], ... }
                  edges=[],  # [(v1, v2), ...]
-                 labels=False,
-                 labels_config={},
-                 vertex_default=Dot(color=BLUE),
+                 label_type=Text,
+                 label_default={"font": "futura"},  # default parameters for labels
+                 label_config={},  # adds/changes the default label parameters, for specific vertices
+                 vertex_default=Dot(color=BLUE), # default vertices
                  vertex_config={},  # key: vertex, value: associating object
+                 vertex_and_label_scale=1,
                  edge_default=Line(),
                  edge_config={},
                  **kwargs
@@ -18,15 +20,6 @@ class BasicGraph(VMobject):
 
         # turns the positions into an np array
         vertices = {vertex: np.array(pos + [0]) for vertex, pos in vertices.items()}
-
-        # adds vertices
-        for v in vertices:
-            if v in vertex_config:
-                vertex_image = vertex_config[v]
-            else:
-                vertex_image = vertex_default.copy()
-            vertex_image.move_to(vertices[v])
-            self.add(vertex_image)
 
         # adds edges
         for (v1, v2) in edges:
@@ -39,7 +32,7 @@ class BasicGraph(VMobject):
 
         # Text(letter, font="futura") centered with reference to a circle of radius=0.5
         FUTURA_CENTERING_POS = {
-            "A": 0.04 * UP, "B": 0.03 * RIGHT, "C": 0.04 * RIGHT, "D": 0.04 * RIGHT,
+            "A": 0.04 * UP, "B": 0.03 * RIGHT, "C": 0.04 * LEFT, "D": 0.04 * RIGHT,
             "E": 0.01 * DOWN, "F": 0.02 * RIGHT + 0.01 * DOWN, "G": ORIGIN,
             "H": 0.004 * RIGHT, "I": ORIGIN, "J": 0.035 * LEFT, "K": 0.025 * RIGHT,
             "L": 0.025 * RIGHT, "M": 0.02 * UP, "N": ORIGIN, "O": ORIGIN,
@@ -47,6 +40,23 @@ class BasicGraph(VMobject):
             "S": ORIGIN, "T": 0.03 * DOWN, "U": 0.02 * DOWN, "V": 0.045 * DOWN,
             "W": 0.05 * DOWN, "X": 0.01 * DOWN, "Y": 0.03 * DOWN, "Z": ORIGIN
         }
+
+        # adds vertices and labels
+        for v in vertices:
+            if v in vertex_config:
+                vertex_image = vertex_config[v]
+            else:
+                vertex_image = vertex_default.copy()
+
+            if v in label_config:
+                label_image = label_type(text=v, **label_default.update(label_config[v]))
+            else:
+                label_image = label_type(text=v, **label_default)
+
+            vertex_image.scale(vertex_and_label_scale).move_to(vertices[v])
+            label_image.move_to(FUTURA_CENTERING_POS[v]).scale(vertex_and_label_scale).shift(vertices[v])
+            self.add(vertex_image)
+            self.add(label_image)
 
     # transforms graph g1 to g2 (only for removing and adding to the graph)
     @staticmethod

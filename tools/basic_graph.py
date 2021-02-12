5038e792dd3a5a316668fe73508e1f225914741f
Abhinand Shibu <abhinandshibu@outlook.com>
2021-02-12 18:01:57 +0000

    Added quantities letters of futura font must be shifted by, to align in to the middle. const FUTURA_CENTERING_POS

--- tools/basic_graph.py
+++ tools/basic_graph.py
@@ -6,8 +6,8 @@ class BasicGraph(VMobject):
     def __init__(self,
                  vertices={},  # includes position
                  edges=[],  # [(v1, v2), ...]
-                 labels={},
-
+                 labels=False,
+                 labels_config={},
                  vertex_default=Dot(color=BLUE),
                  vertex_config={},  # key: vertex, value: associating object
                  edge_default=Line(),
@@ -36,6 +36,17 @@ class BasicGraph(VMobject):
                 edge_image = edge_default.copy()
             edge_image.put_start_and_end_on(vertices[v1], vertices[v2])
             self.add(edge_image)
+
+        # Text(letter, font="futura") centered with reference to a circle of radius=0.5
+        FUTURA_CENTERING_POS = {
+            "A": 0.04 * UP, "B": 0.03 * RIGHT, "C": 0.04 * RIGHT, "D": 0.04 * RIGHT,
+            "E": 0.01 * DOWN, "F": 0.02 * RIGHT + 0.01 * DOWN, "G": ORIGIN,
+            "H": 0.004 * RIGHT, "I": ORIGIN, "J": 0.035 * LEFT, "K": 0.025 * RIGHT,
+            "L": 0.025 * RIGHT, "M": 0.02 * UP, "N": ORIGIN, "O": ORIGIN,
+            "P": 0.035 * RIGHT + 0.01 * DOWN, "Q": ORIGIN, "R": 0.03 * RIGHT,
+            "S": ORIGIN, "T": 0.03 * DOWN, "U": 0.02 * DOWN, "V": 0.045 * DOWN,
+            "W": 0.05 * DOWN, "X": 0.01 * DOWN, "Y": 0.03 * DOWN, "Z": ORIGIN
+        }
 
     # transforms graph g1 to g2 (only for removing and adding to the graph)
     @staticmethod
